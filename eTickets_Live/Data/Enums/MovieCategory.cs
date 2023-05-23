f65cd364eef1bd73eff0b29dd190b8316c674d48
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-05-23 10:12:48 +0300

    Some changes

---
+++ eTickets_Live/Data/Enums/MovieCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace eTickets_Live.Data.Enums
+{
+    public enum MovieCategory
+    {
+        // "Özel" bir sınıf. Sabit durumda olan verilerin tutulacağı bir sınıf.
+        // Normalde index yapısında 0. index ile başlar. Fakat bunu db tarafında kullanacağım için
+        // 0 dan farklı bir index numarasıyla başlatabiliyorum. Diğer sabitlerde indexlerini otomatik olarak 
+        // ayarlıyorlar.
+        Aksiyon = 1,
+        Komedi,
+        Dram,
+        Belgesel,
+        Animasyon,
+        Korku
+
+
+    }
+}
---
+++ eTickets_Live/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using eTickets_Live.Data.Enums;
+using System.Data;
+
+namespace eTickets_Live.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageURL { get; set; } // Filmin afişi - int sitesinden gelecek şekilde varsayım
+
+        public DateTime StartDate { get; set; } // Vizyona giriş tarihi
+
+        public DateTime EndDate { get; set; } // Vizyondan kalkış tarihi
+
+        // Bu filmin kategorisi ayrı bir özel "enum" olarak yaratıldı
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
