2085c070ada01c3c184f723cecfcd592c8090c4f
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-05-23 14:04:11 +0300

    Some changes

--- eTickets_Live/Data/AppDBInitializer.cs
+++ eTickets_Live/Data/AppDBInitializer.cs
@@ -231,18 +231,16 @@ namespace eTickets_Live.Data
                             ActorId = 3,
                             MovieId = 1
                         },
-
-                         new Actor_Movie()
+                        new Actor_Movie()
                         {
                             ActorId = 1,
                             MovieId = 2
                         },
-                         new Actor_Movie()
+                        new Actor_Movie()
                         {
                             ActorId = 4,
                             MovieId = 2
                         },
-
                         new Actor_Movie()
                         {
                             ActorId = 1,
@@ -258,8 +256,6 @@ namespace eTickets_Live.Data
                             ActorId = 5,
                             MovieId = 3
                         },
-
-
                         new Actor_Movie()
                         {
                             ActorId = 2,
@@ -275,8 +271,6 @@ namespace eTickets_Live.Data
                             ActorId = 4,
                             MovieId = 4
                         },
-
-
                         new Actor_Movie()
                         {
                             ActorId = 2,
@@ -297,8 +291,6 @@ namespace eTickets_Live.Data
                             ActorId = 5,
                             MovieId = 5
                         },
-
-
                         new Actor_Movie()
                         {
                             ActorId = 3,
