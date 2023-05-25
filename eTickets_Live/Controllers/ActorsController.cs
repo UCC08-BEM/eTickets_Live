b045654d1841d540140979291c4fccf997d65408
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-05-25 09:32:30 +0300

    Some changes

---
+++ eTickets_Live/Controllers/ActorsController.cs
@@ -0,0 +1,26 @@
+﻿using eTickets_Live.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets_Live.Controllers
+{
+    public class ActorsController : Controller
+    {
+        // Bu controller ile öncelikle db tarafındaki verleri görüntüleyelim.
+        // dbcontext tanımlarını yapmam gerekiyor.
+
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+
+        public IActionResult Index()
+        {
+            var actorsdata = _context.Actors.ToList();
+
+            return View(actorsdata);
+        }
+    }
+}
