beb5cb16196fae7baaa0a2bbd226b58ad49e8bba
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-05-26 14:13:05 +0300

    Some changes

--- eTickets_Live/Controllers/ActorsController.cs
+++ eTickets_Live/Controllers/ActorsController.cs
@@ -63,5 +63,64 @@ namespace eTickets_Live.Controllers
 
             return View(actorDetails);
         }
+
+        // Get: Actors/Edit/1
+        public IActionResult Edit(int id)
+        {
+
+            var actorDetails = _service.GetById(id);
+
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id,[Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _service.Update(id, actor);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Get: Actors/Delete/1
+        public IActionResult Delete(int id)
+        {
+
+            var actorDetails = _service.GetById(id);
+
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(actorDetails);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+
+            var actorDetails = _service.GetById(id);
+
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            _service.Delete(id);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
