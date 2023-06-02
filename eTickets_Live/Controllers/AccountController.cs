682b8b3ab285f4c931c9a5cd9c2af69bc84611f3
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-06-02 10:22:10 +0300

    Some changes

---
+++ eTickets_Live/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using eTickets_Live.Data;
+using eTickets_Live.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets_Live.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInuser;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInuser, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInuser = signInuser;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ eTickets_Live/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets_Live.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name ="E-Mail :")]
+        [Required(ErrorMessage ="EMail alanı gereklidir..")]
+        public string EMailAddress { get; set; }
+        
+        [Display(Name ="Şifre :")]
+        [Required(ErrorMessage ="Şifrenizi girmeniz gerekmektedir..")]
+        public string Password { get; set; }
+    }
+}
